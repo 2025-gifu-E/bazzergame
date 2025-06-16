e84acfbf0ea6effbbac1f61408d1f1732b23e861
rangea100 <hinara1203@gmail.com>
2025-06-16 22:00:52 +0900

    ボタンにテクスチャーをさらに追加

--- MiniGame/rangea_lightsout/scean/lightsout_main/lightsout_main.gd
+++ MiniGame/rangea_lightsout/scean/lightsout_main/lightsout_main.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends Control
 
-const GRID_SIZE = 5
+const GRID_SIZE = 3
 @onready var grid_container = $GridContainer
 var grid = []
 var ButtonScene = preload("res://MiniGame/rangea_lightsout/scean/button/button.tscn")  # プレハブボタン
@@ -13,13 +13,12 @@ func _ready():
 			var btn = ButtonScene.instantiate()
 			btn.toggle_mode = true
 			btn.custom_minimum_size = Vector2(64, 64)
-			btn.text = ""
 			btn.pressed.connect(_on_button_pressed.bind(x, y))
 			grid_container.add_child(btn)
 			grid[y].append(btn)
 
 func _on_button_pressed(x: int, y: int) -> void:
-	_toggle(x, y)
+	_toggle(x, y)           # 押されたボタン自身
 	_toggle(x - 1, y)
 	_toggle(x + 1, y)
 	_toggle(x, y - 1)
@@ -31,7 +30,7 @@ func _on_button_pressed(x: int, y: int) -> void:
 func _toggle(x: int, y: int) -> void:
 	if x >= 0 and x < GRID_SIZE and y >= 0 and y < GRID_SIZE:
 		var btn = grid[y][x]
-		btn.button_pressed = !btn.button_pressed
+		btn.toggle_pressed()
 
 func _check_cleared() -> bool:
 	for row in grid:
