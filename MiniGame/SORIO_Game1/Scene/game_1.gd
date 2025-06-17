0efe3d6f5cc1b56a227ac85246900fb4137b8a12
S0RI0 <alexitypeeez@gmail.com>
2025-06-18 00:43:05 +0900

    一旦完成

--- MiniGame/SORIO_Game1/Scene/game_1.gd
+++ MiniGame/SORIO_Game1/Scene/game_1.gd
@@ -2,11 +2,15 @@ extends Node2D
 @onready var ufo: CharacterBody2D = $UFO
 @onready var left_arm: CharacterBody2D = $UFO/CollisionShape2D/left_arm
 @onready var right_arm: CharacterBody2D = $UFO/CollisionShape2D/right_arm
+@onready var rigid_body_2d: RigidBody2D = $RigidBody2D
 
-var arm_max_angle:float = -56
-var arm_min_angle:float = -30
+
+
+
+const arm_max_angle:float = -45
+const arm_min_angle:float = -30
 var Crane_Pos:Vector2
-var Mover:float = 1
+var Mover:float = 20
 
 enum Crane {
 	Move,Down,Close,Up,Go,Open,End
@@ -19,41 +23,42 @@ func _ready() -> void:
 	right_arm.rotation_degrees = arm_min_angle * -1
 	ufo.position = Vector2(0,-250)
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	match GameFlow:
 		Crane.Move:
-			print(Mover)
 			if(Crane_Pos.x <= -400):
-				Mover = 1
+				Mover = 20
 			if(Crane_Pos.x >= 400):
-				Mover = -1
+				Mover = -20
 			Crane_Pos.x += Mover
 			if(Input.is_action_just_pressed("enter")):
 				GameFlow = Crane.Down
 		
 		Crane.Down:
-			Crane_Pos.y += 0.25
+			Crane_Pos.y += 5
 			if(Crane_Pos.y >= 400):
 				GameFlow = Crane.Close
 		
 		Crane.Close:
-			create_tween().tween_property(left_arm,"rotation_degrees",arm_max_angle,1)
-			create_tween().tween_property(right_arm,"rotation_degrees",arm_max_angle * -1,1)
+			create_tween().tween_property(left_arm,"rotation_degrees",arm_max_angle,0.4)
+			create_tween().tween_property(right_arm,"rotation_degrees",arm_max_angle * -1,0.4)
 			await get_tree().create_timer(1).timeout
 			GameFlow = Crane.Up
 		
 		Crane.Up:
-			Crane_Pos.y -= 0.25
+			Crane_Pos.y -= 5
 			if(Crane_Pos.y <= 0):
 				GameFlow = Crane.Go
 		
 		Crane.Go:
-			Crane_Pos.x += 0.5
+			Crane_Pos.x += 5
 			if(Crane_Pos.x >= 400):
 				GameFlow = Crane.Open
 		
 		Crane.Open:
-			create_tween().tween_property(left_arm,"rotation_degrees",arm_min_angle,1)
-			create_tween().tween_property(right_arm,"rotation_degrees",arm_min_angle * -1,1)
+			create_tween().tween_property(left_arm,"rotation_degrees",arm_min_angle,0.4)
+			create_tween().tween_property(right_arm,"rotation_degrees",arm_min_angle * -1,0.4)
+			await get_tree().create_timer(1).timeout
+			GameFlow = Crane.Move
 		
 	ufo.position = Crane_Pos
