be8b6ebc2b871d31b33a1520d71aa0eb10ab5f50
soripia <soripia25@gmail.com>
2025-06-21 11:29:58 +0900

    タイマー追加　soripia

--- MiniGame/syoma_game1/mainn.gd
+++ MiniGame/syoma_game1/mainn.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
-
-
+@onready var timer = $time
 @onready var _label = $gameclear
+@onready var _lavel2 = $gameover
+
+func _ready() -> void:
+	timer.time_start()
+
 func _process(delta: float) -> void:
 	var cnt = 0
 	for child in get_children():
@@ -10,3 +14,10 @@ func _process(delta: float) -> void:
 			cnt += 1
 	if cnt == 0:
 		_label.visible = true
+
+
+func _on_time_time_out() -> void:
+	_lavel2.visible = true
+	
+	
+	
